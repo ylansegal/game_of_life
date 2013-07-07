934f70e60ddfea8c37296dd3c29495fbb13cc44a
Ylan Segal <ylan.segal@mellmo.com>
2013-07-07 08:08:11 -0700

    Implement Grid#adjacent

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,5 +1,5 @@
 require "game_of_life/version"
+require 'game_of_life/grid'
 
 module GameOfLife
-  # Your code goes here...
 end
---
+++ lib/game_of_life/grid.rb
@@ -0,0 +1,20 @@
+require 'matrix'
+
+module GameOfLife
+  class Grid < Matrix
+
+    def adjacent(row, column)
+      x_min, x_max = [row - 1, 0].max, [row + 1, column_vectors.size - 1].min
+      y_min, y_max = [column - 1, 0].max, [column + 1, row_size - 1 ].min
+
+      result = []
+      (x_min..x_max).each do |x|
+        (y_min..y_max).each do |y|
+          result << self[x, y] unless x == row && y == column
+        end
+      end
+
+      result
+    end
+  end
+end
