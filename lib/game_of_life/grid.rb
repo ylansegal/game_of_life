991329a58ae694e26b06a23fb7763b217eeb88b0
Ylan Segal <ylan.segal@mellmo.com>
2013-07-14 21:20:26 -0700

    Refactor for clarity

--- lib/game_of_life/grid.rb
+++ lib/game_of_life/grid.rb
@@ -4,7 +4,7 @@ module GameOfLife
   class Grid < Matrix
 
     def adjacent(row, column)
-      x_min, x_max = [row - 1, 0].max, [row + 1, column_vectors.size - 1].min
+      x_min, x_max = [row - 1, 0].max, [row + 1, column_size - 1].min
       y_min, y_max = [column - 1, 0].max, [column + 1, row_size - 1 ].min
 
       result = []
