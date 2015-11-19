7eafcc206422845753909c4d59f4e6589504f8d8
Ylan Segal <ylan@segal-family.com>
2015-11-19 09:34:45 -0800

    Refactor to use built-in matrix operations

--- lib/game_of_life/grid.rb
+++ lib/game_of_life/grid.rb
@@ -7,14 +7,7 @@ module GameOfLife
       x_min, x_max = [row - 1, 0].max, [row + 1, column_size - 1].min
       y_min, y_max = [column - 1, 0].max, [column + 1, row_size - 1 ].min
 
-      result = []
-      (x_min..x_max).each do |x|
-        (y_min..y_max).each do |y|
-          result << self[x, y] unless x == row && y == column
-        end
-      end
-
-      result
+      minor(x_min..x_max, y_min..y_max).to_a.flatten - [self[row, column]]
     end
 
     def to_s
