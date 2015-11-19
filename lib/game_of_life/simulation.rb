ee28773b09814a8d05e40306e579e6dc5ecffea0
Ylan Segal <ylan@segal-family.com>
2015-11-19 10:58:43 -0800

    Move simulation rules to smaller methods for clarity

--- lib/game_of_life/simulation.rb
+++ lib/game_of_life/simulation.rb
@@ -1,30 +1,30 @@
 module GameOfLife
   class Simulation
+    def next_grid_generation(grid)
+      Grid.build(grid.row_size, grid.column_size) { |row, column|
+        next_cell_generation(grid[row, column], grid.adjacent(row, column))
+      }
+    end
+
     def next_cell_generation(cell, neighbours)
       live_count = neighbours.count { |item| item.is_a?(LiveCell) }
 
       case cell
       when LiveCell
-        if live_count < 2
-          DeadCell.new
-        elsif live_count >= 2 && live_count <= 3
-          LiveCell.new
-        else
-          DeadCell.new
-        end
+        next_live_cell_generation(live_count)
       when DeadCell
-        if live_count == 3
-          LiveCell.new
-        else
-          DeadCell.new
-        end
+        next_dead_cell_generation(live_count)
       end
     end
 
-    def next_grid_generation(grid)
-      Grid.build(grid.row_size, grid.column_size) { |row, column|
-        next_cell_generation(grid[row, column], grid.adjacent(row, column))
-      }
+    private
+
+    def next_live_cell_generation(live_count)
+      live_count == 2 || live_count == 3 ? LiveCell.new : DeadCell.new
+    end
+
+    def next_dead_cell_generation(live_count)
+      live_count == 3 ? LiveCell.new : DeadCell.new
     end
   end
 end
